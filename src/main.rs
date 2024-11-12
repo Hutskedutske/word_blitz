e335a60bcfb9b965b82e36a2db31e576b8d03469
Cas D'Hollander <cas.dhollander@ugent.be>
2024-11-12 22:14:03 +0100

    alignment fix

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,19 @@ fn recursive_search(visited_coords: &mut Vec<(i32, i32)>, length: usize) {
     if length < 3 {
         for x in -1..2 {
             for y in -1..2 {
+                
                 let lastcoord: &(i32, i32) = match visited_coords.get(length - 1) {
                     Some(el) => el,
                     None => &(0, 0),
                 };
 
-                if 5 > lastcoord.0 + x
+                if     lastcoord.0 + x < 5
+                    && lastcoord.1 + y < 5
                     && lastcoord.0 + x >= 0
-                    && 5 > lastcoord.1 + y
                     && lastcoord.1 + y >= 0
                     && visited_coords.contains(&(lastcoord.0 + x, lastcoord.1 + y))
                 {
-                    visited_coords.push((x, y));
+                    visited_coords.push((lastcoord.0 + x, lastcoord.1 + y));
                     recursive_search(visited_coords, length + 1);
                     visited_coords.remove(length);
                 }
