37fa5cd3fead693be57e85495e54ff5bcef52d19
Cas D'Hollander <cas.dhollander@ugent.be>
2024-11-12 22:10:55 +0100

    start for recursive word seeker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::vec;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn word_seeker(grid: [[char; 4]; 4]) -> Vec<String> {
+    for x in 0..5 {
+        for y in 0..5 {
+            let mut coords = vec![(x, y)];
+            recursive_search(&mut coords, 1);
+        }
+    }
+
+    Vec::new()
+}
+
+fn recursive_search(visited_coords: &mut Vec<(i32, i32)>, length: usize) {
+    if length < 3 {
+        for x in -1..2 {
+            for y in -1..2 {
+                let lastcoord: &(i32, i32) = match visited_coords.get(length - 1) {
+                    Some(el) => el,
+                    None => &(0, 0),
+                };
+
+                if 5 > lastcoord.0 + x
+                    && lastcoord.0 + x >= 0
+                    && 5 > lastcoord.1 + y
+                    && lastcoord.1 + y >= 0
+                    && visited_coords.contains(&(lastcoord.0 + x, lastcoord.1 + y))
+                {
+                    visited_coords.push((x, y));
+                    recursive_search(visited_coords, length + 1);
+                    visited_coords.remove(length);
+                }
+            }
+        }
+    }
 }
