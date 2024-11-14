d9e81cdaf077f8ec890e5ada1e1abbe37cd86aeb
Cas D'Hollander <cas.dhollander@ugent.be>
2024-11-14 12:20:05 +0100

    added comments

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,29 @@ use std::{
     collections::HashSet, fs::File, io::{self, BufRead, BufReader}, str::Chars, vec
 };
 
-use rusqlite::Connection;
-
 fn main() {
+
+    //initialize wordlist and paths
     let mut wordlist: HashSet<String> = HashSet::new();
     let _ = fill_set_file(&mut wordlist);
     let paths: Vec<Vec<(i32, i32)>> = pathfinder();
 
+    //execution loop
     loop {
         let mut woord: String = String::new();
 
+        //input
         println!("Enter the grid: ");
 
-    io::stdin().read_line(&mut woord)
+        io::stdin().read_line(&mut woord)
         .expect("Failed to read line");
 
+        //removes \n chars from input
         woord = String::from(woord.trim());
 
         let grid: [[char; 4]; 4] = grid_builder(woord);
 
+        //finds every word and prints it out if it is valid
         for path in &paths {
             let woord: String = word_builder(grid, path);
             if wordlist.contains(&woord){
@@ -31,25 +35,22 @@ fn main() {
     }
 }
 
+/**Builds a grid from a string and checks if it is valid */
 fn grid_builder(woord: String) -> [[char; 4]; 4] {
-    println!("{}", woord.len());
-
+    //checks if grid is valid
     if woord.len() != 16 {
         panic!("grid is niet 16 characters lang");
     }
 
     let capwoord = woord.to_ascii_uppercase();
 
-    let chars = capwoord.chars();
-
-    for char in chars {
+    for char in capwoord.chars() {
         if !char.is_alphabetic() {
-            panic!("niet alle chars zijn letters")
+            panic!("niet alle chars zijn letters");
         }
     }
 
-    let mut chars: Chars<'_> = woord.chars();
-
+    //initialize grid
     let mut grid: [[char; 4]; 4] = [
         [' ', ' ', ' ', ' '],
         [' ', ' ', ' ', ' '],
@@ -57,6 +58,8 @@ fn grid_builder(woord: String) -> [[char; 4]; 4] {
         [' ', ' ', ' ', ' '],
     ];
     
+    //creates iterator over characters from the string to put in the grid
+    let mut chars: Chars<'_> = woord.chars();
 
     for x in 0..4 {
         for y in 0..4 {
@@ -67,13 +70,17 @@ fn grid_builder(woord: String) -> [[char; 4]; 4] {
     grid
 }
 
+/**Calls recursive_pathfinder on every coordinate to get every path and returns them in a list */
 fn pathfinder() -> Vec<Vec<(i32, i32)>> {
     let mut all_paths: Vec<Vec<(i32, i32)>> = Vec::new();
 
+    //loop over every starting point
     for x in 0..4 {
         for y in 0..4 {
             let mut coords = vec![(x, y)];
             let mut paths: Vec<Vec<(i32, i32)>> = Vec::new();
+
+            //find all paths from a starting point and add them to all paths
             recursive_pathfinder(&mut coords, &mut paths, 1);
             all_paths.append(&mut paths);
         }
@@ -81,26 +88,31 @@ fn pathfinder() -> Vec<Vec<(i32, i32)>> {
     all_paths
 }
 
+/**Constructs all paths of length 1-5 from a given starting point */
 fn recursive_pathfinder(
     visited_coords: &mut Vec<(i32, i32)>,
     paths: &mut Vec<Vec<(i32, i32)>>,
     length: usize,
 ) {
+    //add current coordinates to visited coordinates
     paths.push(visited_coords.to_vec());
     if length < 5 {
         for x in -1..2 {
             for y in -1..2 {
+                //None case never occurs
                 let lastcoord: &(i32, i32) = match visited_coords.get(length - 1) {
                     Some(el) => el,
                     None => &(0, 0),
                 };
 
+                //checks if coordinate is in the grid or if it has veen visited before
                 if lastcoord.0 + x < 4
                     && lastcoord.1 + y < 4
                     && lastcoord.0 + x >= 0
                     && lastcoord.1 + y >= 0
                     && !visited_coords.contains(&(lastcoord.0 + x, lastcoord.1 + y))
                 {
+                    //recursive backtracking
                     visited_coords.push((lastcoord.0 + x, lastcoord.1 + y));
                     recursive_pathfinder(visited_coords, paths, length + 1);
                     visited_coords.remove(length);
@@ -110,6 +122,7 @@ fn recursive_pathfinder(
     }
 }
 
+/**Constructs a string from a path in the grid */
 fn word_builder(grid: [[char; 4]; 4], path: &Vec<(i32, i32)>) -> String {
     let mut word: String = String::new();
     for coord in path {
@@ -118,10 +131,12 @@ fn word_builder(grid: [[char; 4]; 4], path: &Vec<(i32, i32)>) -> String {
     word
 }
 
+/**Fills a HashSet<String> with words from a file*/
 fn fill_set_file(set: &mut HashSet<String>) {
+    
     let filepath: &str = "resources/woordenlijst-5.txt";
 
-    let file = match File::open(filepath) {
+    let file: File = match File::open(filepath) {
         Ok(file) => file,
         Err(_) => panic!("file could not be opened"),
     };
